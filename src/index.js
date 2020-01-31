62532ad0fcc4520deb7bf17b3e74531f21cf8afc
Wilson Romero <wilson.romero@gmail.com>
2020-01-31 09:46:27 -0500

    Solucion al reto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const trialDivision = (number) => {
-    
-  }
+Number.prototype.isInteger = function() {
+  const n = this.valueOf();
+  return parseInt(n) === n;
+};
 
-  module.exports = trialDivision;
+const trialDivision = number => {
+  if (!number.isInteger()) return false;
+  for (let i = 2, s = Math.sqrt(number); i <= s; i++)
+    if (number % i === 0) return false;
+  return number > 1;
+};
+
+module.exports = trialDivision;
