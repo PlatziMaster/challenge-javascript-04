c8f98c909dcb207878bbfcbca70bc704c65a9032
Jefferson Aguilar <jefferson@alunices.com>
2020-02-05 23:02:51 -0500

    Implementación de función para saber si un determinado número es primo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
+  let interaction = 1;
+  let zeroWastedivision = 0;
+  while(interaction <= number ) {
+    if(number % interaction == 0) {
+        zeroWastedivision = zeroWastedivision + 1;
+    }
+    interaction = interaction + 1;
   }
+  return zeroWastedivision == 2;
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
