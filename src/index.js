ba7477b8f2dc27faab9edf0051df2e14be1ed963
eriksape <erik.sape@gmail.com>
2020-02-06 04:15:14 -0600

    solving prime number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-    
+  number = parseInt(number);
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+        return false;
+    }
   }
+  return number > 1;
+}
 
-  module.exports = trialDivision;
+
+module.exports = trialDivision;
