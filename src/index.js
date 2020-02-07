e791b97c49172ffe80541f639e8a66bd0cebb428
Jorge Humberto Nemoga Pinzon <jhnemogap@gmail.com>
2020-02-06 22:13:19 -0500

    Solved trialDivision

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-    
+  let isPrime = false;
+  if (Number.isInteger(number)) {
+    if (number > 1) {
+      if (number % 2 === 1 || number === 2) {
+        isPrime = true;
+        let divider = 3;
+        while (divider <= Math.floor(number / 2)) {
+          divider += 2;
+          Number.isInteger(number / divider) ? isPrime = false : '';
+        }
+      }
+    }
   }
+  return isPrime;
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
