43e99df6cae347ea9b695c4e2295f16c07676cc7
dfloresdev <hello@dflores.dev>
2020-02-08 19:18:58 -0600

    Mission complete

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-const trialDivision = (number) => {
-    
-  }
+const trialDivision = number => {
+  if (number > 0 && Number.isInteger(number)) {
+    for (var i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
 
-  module.exports = trialDivision;
+    if (number === 1) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
+
+module.exports = trialDivision;
