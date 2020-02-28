7aa3acebe139b8bf0381ccfaa131327873266402
Erinor <ericksn0707@gmail.com>
2020-02-27 19:55:59 -0600

    Correccion numero 5

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const trialDivision = (number) => {
-    
-  }
+const trialDivision = number => {
+    let z = 0;
+    if (number == 1 || number < 1 || number % 1 != 0) return false;
+    for (let i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            z++;
+        }
+    }
+    if (z > 2) {
+        return false;
+    } else {
+        return true;
+    }
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
