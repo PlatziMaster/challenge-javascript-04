106a639320166cc15b2a8b80e7a0cb42062ff3c8
Kristian Lopez <kristian.lom@gmail.com>
2020-03-04 12:45:48 -0600

    Reto solucionado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const trialDivision = (number) => {
-    
-  }
 
-  module.exports = trialDivision;
+  return (!
+    ((number <= 1)
+      || (!Number.isInteger(number)
+        || ((number > 2 && (number % 2) == 0)
+          || (number > 3 && (number % 3) == 0)
+          || (number > 5 && (number % 5) == 0)
+          || (number > 7 && (number % 7) == 0)
+        )
+      )
+    )
+  );
+}
+
+module.exports = trialDivision;
