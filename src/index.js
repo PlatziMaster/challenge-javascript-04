25bc4100f6e2683f9c44a23e2e4554969a6e38a6
David Behar <david@behar.io>
2020-03-09 01:53:06 -0600

    Change folder structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-const trialDivision = (number) => {
-    
-  }
+const prompt=require('prompt')
+const trialDivision = require('./utils/trialDivision');
 
-  module.exports = trialDivision;
+prompt.start();
+console.log('Ingresa un número que desees saber su fibonacci: ')
+prompt.get(['number'], function (err, result) {
+  console.log('  Número: ' + result.number);
+  console.log('  fibonacci: ' + trialDivision(result.number));
+});
