3d03f1a3d5e94245a18b47c2d03f04f7a86e83a6
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-03-21 12:32:45 -0600

    reto cumplido, numeros primos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const trialDivision = (number) => {
-    
-  }
+    let r = 0
 
-  module.exports = trialDivision;
+    if (number == 0 || number == 1) {
+        return false
+    } else {
+
+        for (let i = number; i > 0; i--) {
+            if (number % i === 0) {
+                r++
+                if (r > 2) {
+                    return false
+                }
+            }
+        }
+        return true
+    }
+}
+
+module.exports = trialDivision;
