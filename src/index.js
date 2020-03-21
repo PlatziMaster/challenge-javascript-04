2d25ed3e6008edcf9504196b10306c0790af8f80
Juan Jose Mayorga <tmaniacus@gmail.com>
2020-03-21 12:53:22 -0600

    reto cumplido, corregido

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const trialDivision = (number) => {
     let r = 0
 
-    if (number == 0 || number == 1 || Math.sign(number) == -1 || Math.sign(number) == -0) {
+    if (!Number.isInteger(number)) {
         return false
     } else {
+        if (number == 0 || number == 1 || Math.sign(number) == -1 || Math.sign(number) == -0) {
+            return false
+        } else {
 
-        for (let i = number; i > 0; i--) {
-            if (number % i === 0) {
-                r++
-                if (r > 2) {
-                    return false
+            for (let i = number; i > 0; i--) {
+                if (number % i === 0) {
+                    r++
+                    if (r > 2) {
+                        return false
+                    }
                 }
             }
+            return true
         }
-        return true
     }
 }
 
