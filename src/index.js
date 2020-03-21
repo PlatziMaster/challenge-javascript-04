2e3271882c1be7e7f21a5ba1c16226a90a804ec2
Andres Alba <tensinkusum@hotmail.com>
2020-03-21 16:04:14 -0500

    reto 4 javascript Andres Alba

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const trialDivision = (number) => {
-    
+
+  var cont = 0;
+
+  if(number <= 1)
+  {
+    return false;  
+  }
+  else{
+    for(var i = 2; i<=number; i++){
+      if(number%i === 0){
+        cont++;
+      }
+    }
+	
+  	if(cont===1){
+		  return true;
+	  }
+	  else{
+		  return false;
+    }
+  }
+
   }
 
   module.exports = trialDivision;
