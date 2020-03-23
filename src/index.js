21656177c30c95c40f600186c83df5380652cf89
L. Felipe Carrillo A <felipe.carrillo1@hotmail.com>
2020-03-22 21:00:28 -0600

    Solución al problema

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
-    
+    var div=0;
+    for(let i=0; i<=number; ++i){
+      if(number%i==0) div+=1;
+    }
+    if(div==2) return true;
+    else return false;
   }
 
   module.exports = trialDivision;
