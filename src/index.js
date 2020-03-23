7281f184f38135106c4e03bf82e7ed60b8faac54
Armando <rivera.armando997@gmail.com>
2020-03-23 13:39:56 -0600

    Prueba de primalidad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
-    
-  }
+	
+	let primos = [2, 3, 5];
+
+	if (number < 2 || number % 1 != 0)
+		return false;
+
+	if (primos.includes(number))
+		return true;
+
+	if (number%2 == 0 || number%3 == 0 || number%5 == 0)
+		return false;
+
+	return true;
+
+
+}
 
   module.exports = trialDivision;
