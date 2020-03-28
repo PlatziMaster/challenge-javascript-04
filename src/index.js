b8bbe061dc640e2f474b4cf72d6e8189a373ce30
Alejandro Lopez <alejozepol@gmail.com>
2020-03-28 11:36:22 -0500

    Algoritmo para determinar si un numero es primo o no

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+    if (number <= 1 || number % 1 ) {
+      return false
+    } else {
+      let square = Math.sqrt(number)
+       for (let index = 2; index <= square; index++) {
+        if(number % index === 0){
+         return false
+        }
+       }
+       return true
+    }
   }
-
   module.exports = trialDivision;
