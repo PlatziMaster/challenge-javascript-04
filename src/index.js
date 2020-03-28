7b315b5d9a6e52dbdb5d68a30d83fa63b07327d7
Ruben Padilla <rubbenpad2@gmail.com>
2020-03-28 17:31:03 -0500

    challenge-javascript-04 solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
-  }
+    const limit = Math.floor(Math.sqrt(number))
+
+    for (let i = 2; i <= limit; i++) {
+        if (number % i === 0) {
+            return false
+        }
+    }
+
+    return Number.isInteger(number) && number > 1
+}
 
   module.exports = trialDivision;
