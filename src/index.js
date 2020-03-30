56348c6ec220bfb511dbcbd3c616b9548c6ec3ee
Jonattan Infante Vargas <infanpearl@gmail.com>
2020-03-29 22:07:34 -0500

    Propuesta de solución a numeros primos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 const trialDivision = (number) => {
+    let C,D
+    if (number <= 0) {
+      return false
+    }else{
+      C = 1
+      D = 0
+      while (C<= number) {
+        if (number % C == 0) {
+          D++
+          C++
+        }else{
+          C++
+        }
+        
+      }      
+        
+      if (D == 2) {
+        return true
+      }else{
+        return false
+      }
+    }
+
     
   }
 
