3c3351b86f374d7604e02b44c0c5aaa844d3bfdf
Jared Ortega <jaredo@worcket.com>
2020-03-30 20:30:48 -0600

    Feedback resolved

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const trialDivision = (number) => {
   if (number <= 1 || number % 1) return false;
-  let m = Math.sqrt(number);
-  for (let i = 2; i <= m; i++) {
+  let limit = Math.sqrt(number);
+  for (let i = 2; i <= limit; i++) {
     if (number % i == 0) return false;
   }
   return true;
