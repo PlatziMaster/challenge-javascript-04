a0abffa12c9d695405527781dc1cad81d54bbffd
J. Alvarez <jcalvarezj@gmail.com>
2020-04-02 17:35:47 -0500

    Solved Challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const trialDivision = (number) => {
-    
+const trialDivision = (number) => {  
+  if (number <= 1 || String(number).match(/^\d+\.\d+$/))
+    return false;
+  else {
+    let otherDivisors = false;
+    let currentNumber = 2;
+    while (!otherDivisors && currentNumber < number) {
+      otherDivisors = number % currentNumber === 0;
+      currentNumber++;
+    }
+    return !otherDivisors;
   }
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
