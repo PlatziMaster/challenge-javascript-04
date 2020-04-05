12330ae1139077c96fc27a1e16bb29ce51354b24
Jose Cuevas <josecueram@gmail.com>
2020-04-04 23:59:34 -0600

    resolved challenge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const trialDivision = (number) => {
-    
+  
+  let flooredNumber = Math.floor(number)
+  
+  if (flooredNumber <= 1) {
+    return false
   }
+  let counter = 0
+
+  for (let i = flooredNumber; i > 0; i--) {
+    if ((flooredNumber%i) === 0) {
+      counter++
+    }
+  }
+
+  if(counter > 2) {
+    return false
+  }
+
+  return true
+}
 
   module.exports = trialDivision;
