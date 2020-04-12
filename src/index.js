3c4a55ac41175edbed60df182b75f8b473c44b45
hectormr <45437401+HectorMartinezResendiz@users.noreply.github.com>
2020-04-12 13:14:31 -0500

    Resolviendo el challenge de javacript 04

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const trialDivision = (number) => {
-    
+  let j = 0;
+  for (i = 0; i < number; i++) {
+    if (number % i === 0) {
+      // console.log(`${i} x ${number}`);
+      j++
+    }
   }
+  if (j === 1) {
+    return true
+  } else {
+    return false
+  }
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
