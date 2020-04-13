5b0803b189209e1c64ae4c76c6bbec34e8296054
Jhon L Guerrero <jhoguer@gmail.com>
2020-04-12 19:28:21 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-    
+  let maxNumDiv = 2
+
+  if ( number > 1 && Number.isInteger(number) ) {
+    for (let i = 2; i < number; i++) {
+      if ( (number % i) === 0 ) {
+        maxNumDiv++
+        return false
+      }
+    }
+  } else {
+    return false
   }
+
+  return true
+}
 
   module.exports = trialDivision;
