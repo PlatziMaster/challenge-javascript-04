9846a7de023300ec553fe338cabe752c0e4fa103
camilo <velandiacamilo356@gmail.com>
2020-04-26 09:50:16 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-const trialDivision = (number) => {};
-
+const trialDivision = (number) => {
+  if (number > 0 && Number.isInteger(number)) {
+    for (var i = 2; i < number; i++) {
+      if (number % i === 0) {
+        return false;
+      }
+    }
+    if (number === 1) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    return false;
+  }
+};
 module.exports = trialDivision;
