12c84e7d23b88fafa5f78b43dcc25c603dcebac4
Andres Campuzano <campuxano88@gmail.com>
2020-05-01 23:32:49 -0500

    challenge done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
-  }
+	if (number <= 1 || number % 1) {
+		return false;
+	}
+	for (let x = 2; x < number; x++) {
+		if (number % x === 0) {
+			return false;
+		}
+	}
+	return true;
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
