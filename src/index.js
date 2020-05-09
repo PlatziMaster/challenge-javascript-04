12308e276521279ecea05f4123e2cdeea1b964ce
Tonalli López <aguilablanca_7@live.com>
2020-05-09 10:57:53 -0500

    Challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-    
+  if (!Number.isInteger(number) || number < 2) return false;
+
+  const square = Math.sqrt(number);
+
+  for (let i = 2; i <= square; i++) {
+    if (number % i === 0) {
+      return false;
+    }
   }
 
-  module.exports = trialDivision;
+  return true;
+};
+
+module.exports = trialDivision;
