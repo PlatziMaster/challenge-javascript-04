72c133b906c36742063fbebf8475ac4813d92851
agustin <agustin.moranr@gmail.com>
2020-05-11 16:17:18 -0500

    Se resolvio el reto para todos los casos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 const trialDivision = (number) => {
-    
+  let par = 2
+  let module = number % par 
+
+  for(let i = par; number > i; i++) {
+    if(number % i === 0) {
+      return false
+    }
   }
 
-  module.exports = trialDivision;
+  if(number === 1) {
+    return false
+  }
+
+  else if(module === 1 || number === par) {
+    return true
+  }
+
+  else {
+    return false
+  }
+}
+
+module.exports = trialDivision;
+
+
