ecb3a22360bb8954cb1595ec2a6e064bc1e5033e
lenninIbarraGonzalez <soporte@allnexus.com.co>
2020-05-11 15:58:29 -0500

    solucionando reto numero primo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-    
+  if (number < 2 || !Number.isInteger(number)) {
+    return false;
   }
+  let count_denominator = 0;
+  for (i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      count_denominator += 1;
+    }
+  }
+  if (count_denominator === 2) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
