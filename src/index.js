4b5c8b01444d513d23c038eaff5cf4d90ec37b98
gposso <gilberto.posso@gmail.com>
2020-05-11 21:51:15 -0500

    remove unused variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const trialDivision = (number) => {
   if (number <= 1) return false;
   if (!Number.isInteger(number)) return false;
-  let count = 0;
   for (let x = 2; x < number; x++) {
     if (number % x === 0) {
       return false;
