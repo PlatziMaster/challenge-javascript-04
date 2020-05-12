b0e3f00e6bd2ed982c0580cfd854bbe62328f148
gposso <gilberto.posso@gmail.com>
2020-05-11 21:47:19 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
+  if (number <= 1) return false;
+  if (!Number.isInteger(number)) return false;
+  let count = 0;
+  for (let x = 2; x < number; x++) {
+    if (number % x === 0) {
+      return false;
+    }
   }
+  return true;
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
