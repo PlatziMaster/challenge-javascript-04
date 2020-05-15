c1260f37279d235df0da0e552257fba7216ca223
hadiazb <hadiazb@unal.edu.co>
2020-05-15 17:28:50 -0500

    Reto cumplido

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
-const trialDivision = (number) => {
-    
+
+  const trialDivision = (number) => {
+    let cont = 0;
+    if (number <= 1) {
+      return false;
+    } else {
+      for (let i = number; i >=1 ; i--) {
+        if (number % i == 0) {
+          cont++;
+        }
+      }
+      if (cont == 2) {
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
+
 
   module.exports = trialDivision;
