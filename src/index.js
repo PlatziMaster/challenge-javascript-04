018068ae0c94d0719505b4b1a86d8b5afb1d4092
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-05-19 22:13:21 -0500

    Chanllenge solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-    
+  let count = 0;
+  let prime = false;  
+  if (number <=1 ) {
+    return false;
+  } else {
+    for (let i = 1; i <=number; i++) {
+      if (number % i === 0) {
+        count ++;
+      }
+    }
+    count === 2 ? prime = true : primer = false;
   }
+  return prime;
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
