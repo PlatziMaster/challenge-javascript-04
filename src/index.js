b9c1322f0ffb13cb3a354976165ee5bf4bd4d5bb
Alejandro Medina <5almero5@gmail.com>
2020-05-20 15:26:35 -0500

    add TrialDivision Logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const trialDivision = (number) => {
-    
+const trialDivision = (n) => {
+  // casos de auto descarte 
+  if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) {
+    return false;
   }
+  // de 2 a n - 1, evaluamos posibles multiplos
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
   module.exports = trialDivision;
