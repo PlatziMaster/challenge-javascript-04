2a3de51230aa7c96ff48194f33247565bafb9ca1
Stiven Jimenez <sajqprogramming@gmail.com>
2020-05-20 22:46:31 -0500

    challenge completed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+    if(number < 2 || number % 1){
+      return false
+    }else{
+      for(let i = 2; i< number; i++){
+        if(number % i == 0){
+          return false
+        }   
+      }
+    }
+    return true
   }
 
   module.exports = trialDivision;
