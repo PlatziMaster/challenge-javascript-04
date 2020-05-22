35275f398f22a2dc7b306bc6e1f21d1e482a4412
Miguel Ángel <20430676+supermavster@users.noreply.github.com>
2020-05-22 00:21:32 -0500

    [Challenge] Complete JS-04!

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const trialDivision = (number) => {
-    
+  // Convert numbers to int EMC6
+  number = Math.trunc(number)
+  // Check is valid number (property)
+  minPrime = 2
+  if(number < minPrime){
+    return false
   }
+  for (var i = minPrime; i < number; i++) {
+    // Check if the number is divisible by two or more data
+    if (number % i === 0) { 
+      return false;
+    }
+    // Prime: divisible in two
+    // Compound: divisible in three or more
+  }
+  // Check is valid number (property)
+  check = (number !== 1)
+  return check;
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
