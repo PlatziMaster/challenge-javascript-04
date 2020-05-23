8ae79b9a971b560191f9625a22f1a1bcb53cb8f4
Juan Gaybre <jugaybre@gmail.com>
2020-05-23 01:27:25 -0500

    Reto: Prueba de primalidad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-const trialDivision = (number) => {
-    
+const trialDivision = number => {
+  var acum = 0
+  var res = false
+  for(let i=number; i>=2; i--) {
+    if(number % i == 0) {
+      acum++
+    }
   }
+  if(acum == 1) {
+    res = true
+  }
+  return res
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
