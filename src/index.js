ac5803071f2e177770f644ee7ddc3249b5f6b92a
Osvaldo Arzate <osvarz.new@gmail.com>
2020-05-23 13:58:05 -0500

    prueba 01, reto terminado, no hagarra mi terminal por eso lo hago asi

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-    
+  var cantidadDividor = [];
+  for (var i = 1; i <= number; i++) {
+    var resultadoNumber = number / i;
+    var esDivisible = Number.isInteger(resultadoNumber)
+    if (esDivisible === true) {
+      cantidadDividor.push(esDivisible)
+    }
   }
+  if (cantidadDividor.length === 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
