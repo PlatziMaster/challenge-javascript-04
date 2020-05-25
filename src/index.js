21668a108f3018f0e9495c568cc3107d661124c2
Abdiel Ortega <nathanabdiel1@gmail.com>
2020-05-24 21:07:56 -0500

    The test was successful

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+  let primo = true;
+  if (number < 2 || number % 1 !== 0) {
+    return false;
   }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      primo = false;
+    }
+  }
+  return primo;
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
