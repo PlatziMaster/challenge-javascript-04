3a4b8683bab40fb1557dc7f0bf9bccd976679bdc
Razielini <razielco@gmail.com>
2020-05-28 14:28:09 -0600

    Solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
+  let resultado = false
+  if (number <= 1) return resultado
+  let count = 0
+    for(let i = 1; i<= number; i++) {
+      if((number%i) === 0) count++
+    }
+
+    if(count === 2) resultado = true
+    return resultado
   }
 
   module.exports = trialDivision;
