4ebd18662972a1b9e137d687169024d19801ac00
Jhon Carlos Colorado <8047140+jcolorado19@users.noreply.github.com>
2020-05-28 16:02:50 -0500

    Solved challenge Prueba de primalidad

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-    
-  }
+    let result = false;
+    let count = 0
+
+    if(number > 1){
+      for (let i = 1; i <= number; i++) {
+        if((number % i) === 0)
+          count ++
+
+      }
+      if(count === 2)
+        result = true;
+    }
+    return result;
+}
 
   module.exports = trialDivision;
