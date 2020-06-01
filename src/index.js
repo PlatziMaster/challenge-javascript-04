7c36559ef4f47f589a9589eb28b3ad135a2774f2
Jorge Ossa <jorgehossa@gmail.com>
2020-05-31 20:01:44 -0500

    Challenge solved!! ✅

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
-    
+   let counter = 0;
+   for (let i = 1; i <= number; i++) {
+      if (number % i === 0) counter++
+   }
+   if (counter === 2) return true;
+   return false;
   }
 
   module.exports = trialDivision;
