dac8863f11912ee34069928cf0a7bcb5bbd814a8
Jedp <jedp082@gmail.com>
2020-06-02 17:16:24 -0500

    Challenge done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 const trialDivision = (number) => {
-    
+  let dividers = [];
+  for(let i = 1; i <= number; i++){
+    if(number%i === 0){
+      dividers.push(i)
+    }
   }
+  return dividers.length === 2 ? true : false;  
+}
 
+trialDivision(13)
   module.exports = trialDivision;
