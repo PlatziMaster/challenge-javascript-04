d1673efcdd8d2023047d0af7f2416e0099559ce8
Andres Valencia <cryos74@gmail.com>
2020-06-05 21:42:51 -0500

    Challenge completed successfully

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+  if (number <= 1 || number % 1) {
+    return false
   }
+  let m = Math.sqrt(number)
+  for (let i = 2; i <= m; i++) {
+    if (number % i === 0) {
+      return false
+    }
+  }
+  return true
+}
 
   module.exports = trialDivision;
