13b6bc3b15685c30c45fc5bb44e3f9afb3776162
den <den.velez@gmail.com>
2020-06-12 11:15:36 -0500

    challenge's solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const trialDivision = (number) => {
-    
+  let result = 0;
+  for (i = 1; i <= number; i++) {
+    result = number % i === 0 ? result + 1 : result;
   }
+  if (result <= 2) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
