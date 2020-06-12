08e595524e45db52039203c7b6deeb6ba89d2857
den <den.velez@gmail.com>
2020-06-12 11:24:13 -0500

    challenge's solved2

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const trialDivision = (number) => {
   for (i = 1; i <= number; i++) {
     result = number % i === 0 ? result + 1 : result;
   }
-  if (result <= 2) {
+  if (result === 2) {
     return true;
   } else {
     return false;
