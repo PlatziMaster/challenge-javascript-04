0ae685343c906a3e429ee4b7416f2ece532dbd13
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-06-12 11:04:53 -0500

    primality challenged solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const trialDivision = (number) => {
-    
+  if (number <= 1 || number != Math.trunc(number)) {
+    return false;
+  } else if (number > 3) {
+    for (var i = 2; i <= number / 2; i++) if (number % i == 0) return false;
   }
+  return true;
+};
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
