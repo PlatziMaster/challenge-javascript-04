ceaa598c7ec1a6733e5fe36c92e8205fe0301290
CamiloASuarez <camiloandressuarez1@gmail.com>
2020-06-16 12:09:34 -0500

    solution problem c4js. by RedKamo (Camilo Suarez)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const trialDivision = (number) => {
-    
-  }
+const trialDivision=(number)=>{
+  if (number<=1 || number%1) {
+    return false
+}
+
+let m=Math.sqrt(number)
+
+for (let i=2; i<=m; i++) {
+    if (number%i==0) {
+        return false
+    }
+}
+return true
+}
 
   module.exports = trialDivision;
