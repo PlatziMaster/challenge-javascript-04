703ef3ab3bfd10af7ac9103581cf3af370475180
Jesus Mejia <chucho4720013@gmail.com>
2020-06-20 00:55:25 -0500

    Reto completado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const trialDivision = (number) => {
-    
+  if (number >= 2) {
+    let c = 0;
+    for (let i = 2; i <= number; i++) {
+      if ((number % i) === 0) {
+        c++
+      }
+    }
+    if (c === 1) {
+      return true
+    }
+    return false
+  } else {
+    return false
+  }
   }
 
   module.exports = trialDivision;
