b7dc52df8a7420c5d9374fd056e83af275ade632
Diego Rodriguez <alejandro27062@gmail.com>
2020-06-24 14:20:02 -0500

    challenge resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-    
+  if(number <= 1 || number % 1 != 0) {
+    return false
+  } else if(number === 2) {
+    return true
+  } else {
+    for(i = 2; i < number; i++) {
+      if(number % i === 0 ) {
+        return false
+      }
+    }
+    return true
   }
+}
+
 
   module.exports = trialDivision;
