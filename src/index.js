80982f82cbe9fad186deb32cfc7544cb0a2f9651
Natanael <nj.leon.14@gmail.com>
2020-06-29 00:22:10 -0500

    challenge 4 - Done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+  if(number <= 1 || number % 1) {
+    return false;
+  }
+  
+  for (let i = 2; i < number; i++) {
+    if(number % i===0){
+      return false;
+    }
+  }	   
+  return true;     
   }
 
-  module.exports = trialDivision;
+  module.exports = trialDivision
