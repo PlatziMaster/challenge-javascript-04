17eb93aa4fec73e5451c799b6437ece057bf0607
Ulzahk <ulzahk@gmail.com>
2020-07-08 23:54:38 -0500

    Challenge's Answer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const trialDivision = (number) => {
-    
+    if(number === 1 || number < 1){
+      return false
+    } else if ((number === 2) || (number === 3) || (number === 5)){
+      return true
+    } else if ((number % 1 === 0) && (number % number === 0) 
+    && ( number % 2 != 0) && (number % 3 !=0) && (number % 5 !=0)){
+      return true
+    } else {
+      return false
+    }
   }
 
   module.exports = trialDivision;
