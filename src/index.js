6ecee0de133e7d188be3bd978598a7854e919cbd
Daniel Valderrama <dny9919@gmail.com>
2020-07-08 21:23:33 -0500

    resuelto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const trialDivision = (number) => {
-    
+const trialDivision = (n) => {
+  let numero = n;
+  if (numero - Math.floor(numero) == 0) {
+  for (var i = 2; i < n; i++) {
+    if (numero % i === 0) {
+      return false;
+    }
   }
+  if (numero === 1) {
+    return false
+  }
+  if (numero <= 0) {
+    return false
+  } 
+   else {
+      return true
+    }
+  } else{
+    return false
+  }
+}
+trialDivision();
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
