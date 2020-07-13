400f3911c6f6fbe0394e3832d4290f19583a891d
Juan Jose Pelaez <juanhernandez.code@gmail.com>
2020-07-13 18:23:24 -0500

    callenge 4 solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-    
+  if (number % 1 != 0) {
+    return false
+  } else if (number <= 1) {
+    return false
   }
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false
+    }
+  }
+  return true
+}
 
-  module.exports = trialDivision;
+module.exports = trialDivision;
