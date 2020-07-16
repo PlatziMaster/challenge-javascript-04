d1088da919d4280fd579331667a4ba049a8a8361
Jose Garzon <jd.garzon12@gmail.com>
2020-07-16 08:39:28 -0500

    challenge primos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-    
+  let countDivisions = 0
+  for (let i = number; i > 0; i--) {
+    if (number % i === 0) {
+      countDivisions++
+    }
   }
-
+  console.log(countDivisions)
+  if (countDivisions === 2) {
+    return true;
+  } else {
+    return false;
+  }
+}
   module.exports = trialDivision;
