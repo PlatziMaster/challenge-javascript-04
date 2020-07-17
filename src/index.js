3c194773928e5510e9676f8eb73569807a9e49ff
Gerardo Garavito <garavitogerardo@gmail.com>
2020-07-16 20:40:27 -0500

    challenge04 done - gerardogaravito

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const trialDivision = (number) => {
-    
+  var quantity = 0;
+  var i = number;
+
+  while (i > 0){
+    if (number % i === 0){
+      quantity++;
+    }
+    i--;
   }
 
-  module.exports = trialDivision;
+  if (quantity === 2){
+    return true;
+  }else{
+    return false;
+  }
+}
+
+module.exports = trialDivision;
