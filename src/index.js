d4ccc740570a4e3755f9c1a245c772ee82bba455
Alexander Herrera <chander.zap@gmail.com>
2020-07-19 01:51:37 -0500

    reto terminado

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const trialDivision = (number) => {
-    
+  var cont = 0;
+  for (let i = 0; i <= number; i++){
+    if(number % i == 0){
+      cont++;
+    }
   }
+  if(cont == 2){
+    return true;
+  }else{
+    return false;
+  }
+}
 
   module.exports = trialDivision;
