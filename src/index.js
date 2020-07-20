a0fe55c0cdfec194907a23ea6cc6263fe97daf38
Paul Montoya <52388061+PaulMoro@users.noreply.github.com>
2020-07-19 22:28:08 -0500

    Finish Challenge 04 javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const trialDivision = (number) => {
-    
+  var i, divi=0;
+  
+  for(i = 1; i <= number; i++) {
+      
+    if(number % i == 0) {
+      divi++;
+    }
+  } 
+  
+  if(divi == 2) {
+    return true;
+  } else {
+      return false;
   }
-
-  module.exports = trialDivision;
+}
+module.exports = trialDivision;
