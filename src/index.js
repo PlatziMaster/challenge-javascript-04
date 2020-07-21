a55d2215ed34d633ba7a7ce07e82f45b66fab8f1
jose caicedo <joseluiscl@gmail.com>
2020-07-21 17:54:34 -0500

    function validate prime number added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const trialDivision = (number) => {
-    
+let x = false;
+number = parseInt(number)
+  var contador =0
+  if(number>1){
+    if(number== 1){
+      contador += 1
+    }
+    for(i=2;i<number-1;i++){
+      if(number%i==0){
+        contador += 1
+      }
+    }
+    if (contador>0){
+      return x
+    }else {
+      x = true
+      return x
+    }
+  } return x
   }
 
   module.exports = trialDivision;
