cd309782164b2e7e30fe824e82411f73209e0d42
Jaime Escobedo <maildeljimmy@gmail.com>
2020-07-31 00:57:07 -0500

    Reto Numeros Primos

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const trialDivision = (number) => {
-    
+number = parseInt(number)
+  var count = 0
+  if (number === 1) {
+      return false
   }
-
-  module.exports = trialDivision;
+  if (number <= 0) {
+    return false
+  }
+  for ( i=2; i < number-1; i++){
+      if(number%i == 0) {
+          count +=1
+      }
+  }
+      if (count > 0) {
+          return false
+      } else {
+          return true
+      }
+  }
+    module.exports = trialDivision;  
+  
