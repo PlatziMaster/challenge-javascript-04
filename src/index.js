63134d4cd8f17f2fd27fa1293a5d090adf4b853b
Efrain Hernandez <efrainhg2050@gmail.com>
2020-09-16 00:31:04 -0500

    Solucion reto 4 JavaScript PlatziMaster

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const trialDivision = (number) => {
+    let modulo = 0;
+
+    if(number > 1) {
+      let contador = 0;
+
+      for(let i = 1; i <= number; i++) {
+        modulo = number % i; 
+        if(modulo === 0) {
+          contador++;
+        }
+
+      }
+
+      if(contador === 2)
+        return true;
+      
+      if (contador !== 2) 
+        return false;
+      
+    } else 
+        return false;
     
+
   }
 
   module.exports = trialDivision;
