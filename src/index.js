bf9c6dd0531c71861907188f3174cd011768896b
Lidenbrock Ed <stivenh_1224@hotmail.com>
2020-09-22 19:11:24 -0500

    Challenge-javascript-04 Resolved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const trialDivision = (number) => {
-    
+  var count = 0;
+  if (number <=1){
+    return false;
+  } else if(number === 2){
+    return true;
+  } else {
+    for (var i = 2; i <= number; i++) {
+      if(number % i === 0 ){
+        count++;
+      }
+    }
+    if(count === 1) {
+      return true;
+    } else {
+      return false;
+    }  
   }
-
-  module.exports = trialDivision;
+}
+module.exports = trialDivision
